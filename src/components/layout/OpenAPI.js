9f670dbd3ab4e02410919867fab2537d2e2d4bdb
Daniel Bisgrove <daniel@bizz-websites.com>
2025-08-12 16:11:18 -0400

    Update swagger-ui presets.apis call

--- src/components/layout/OpenAPI.js
+++ src/components/layout/OpenAPI.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import SwaggerUi, { presets } from 'swagger-ui'
-import 'swagger-ui/dist/swagger-ui.css'
-import { OpenAPISpec } from '../../global-registry/global-registry-client'
-import PageFrame from './PageFrame'
+import React from 'react';
+import SwaggerUi from 'swagger-ui';
+import 'swagger-ui/dist/swagger-ui.css';
+import { OpenAPISpec } from '../../global-registry/global-registry-client';
+import PageFrame from './PageFrame';
 
 class OpenAPI extends React.Component {
-  componentDidMount () {
+  componentDidMount() {
     SwaggerUi({
       dom_id: '#swaggerContainer',
       url: `${process.env.PUBLIC_URL}${OpenAPISpec}`,
-      presets: [presets.apis]
-    })
+      presets: [SwaggerUi.presets.apis],
+    });
   }
 
-  render () {
+  render() {
     return (
-      <PageFrame title='OpenAPI Specification'>
-        <div id='swaggerContainer' data-testid='openapi-layout'/>
+      <PageFrame title="OpenAPI Specification">
+        <div id="swaggerContainer" data-testid="openapi-layout" />
       </PageFrame>
-    )
+    );
   }
 }
 
-export default OpenAPI
+export default OpenAPI;
